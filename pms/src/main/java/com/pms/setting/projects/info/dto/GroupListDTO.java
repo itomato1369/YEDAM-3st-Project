2093670f4a393f7a399e5ec098fc730db1fccd94
sun88110 <138852390+sun88110@users.noreply.github.com>
2026-03-13 12:31:16 +0900

    stlye : 주석변경

--- pms/src/main/java/com/pms/setting/projects/info/dto/GroupListDTO.java
+++ pms/src/main/java/com/pms/setting/projects/info/dto/GroupListDTO.java
@@ -13,7 +13,7 @@ public class GroupListDTO {
 
     private Long groupNo;       // 그룹 식별 번호
     private String groupName;   // 그룹명
-    private String roleName;    // 📍 핵심: 그룹에 부여된 역할 이름 (예: 기획, 개발 등)
+    private String roleName;    //  핵심: 그룹에 부여된 역할 이름 (예: 기획, 개발 등)
     private Integer memberCount; // 그룹 내 소속 인원 수
     private Integer status;      // 그룹 상태 코드 (510: 활성 등)
     private String statusName;  // 상태명 (활성화/비활성화)
